92023164720db256648acf86c4f76890804037e2
Juraci Vieira <juraci.vieira@gmail.com>
2016-04-27 22:40:11 -0300

    first spec passing

---
+++ test/getObjectWrapper.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+var expect = chai.expect;
+chai.use(sinonChai);
+
+
+describe('getObjectWrapper', function() {
+
+    it('should call s3 with bucket name and key name', function() {
+        var fakeS3 = { getObject: sinon.spy() };
+        var getObjectWrapper = require('../lib/getObjectWrapper.js')(fakeS3);
+        var params = {
+            bucket: 'images',
+            key: 'foo.png'
+        };
+        var callback = function(){};
+
+        getObjectWrapper.get(params, callback);
+        expect(fakeS3.getObject).to.have.been.calledWith(params, callback);
+    });
+});
