0a24cb3a0340a9b9faec2eec7cfac48a3cad9ca7
Juraci Vieira <juraci.vieira@gmail.com>
2016-04-29 23:58:06 -0300

    Checking if the image was resized

--- test/integration/resizer.js
+++ test/integration/resizer.js
@@ -7,18 +7,26 @@ chai.use(sinonChai);
 
 var resizer = require('../../lib/resizer.js');
 var gm = require('gm').subClass({imageMagick: true});
+var largeImage = './test/support/largerThan640.jpg';
 
 describe('resize', function() {
     it('should resize the image larger than 640p', function(done) {
-        this.timeout(30000);
-        var largeImage = './test/support/largerThan640.jpg';
+        this.timeout(10000);
         var readStream = fs.createReadStream(largeImage);
         resizer.resize(readStream, function(err, buffer) {
-            if(err) throw err;
+            expect(err).to.equal(null);
 
             gm(buffer).size(function(err, size) {
                 expect(Math.max(size.width, size.height)).to.be.at.most(640);
                 done();
+
+                /* to actually write the new image
+                var newFile = fs.createWriteStream('./test/support/resised-img.jpg');
+                newFile.write(buffer, function(err) {
+                    if(err) throw err;
+                    done();
+                });
+                */
             });
         });
     });
