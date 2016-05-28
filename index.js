e7fe47e4411247f7a8629801a68cce751b85f973
Juraci Vieira <juraci.vieira@gmail.com>
2016-05-28 18:12:53 -0300

    Resizing working at amazon

---
+++ index.js
@@ -0,0 +1,46 @@
+/*jslint node: true */
+"use strict";
+
+var fileParser = require('./lib/file-parser.js');
+var s3Wrapper  = require('./lib/s3-wrapper.js');
+var resizer    = require('./lib/resizer.js');
+
+exports.handler = function(event, context, callback) {
+    context.callbackWaitsForEmptyEventLoop = false;
+    var s3Object = event.Records[0].s3.object;
+    var bucketName = event.Records[0].s3.bucket.name;
+
+    console.info('Object name: ' + s3Object.key);
+
+    if(!fileParser.isImage(s3Object.key)) {
+        console.info('File is not an image: ' + s3Object.key);
+        callback(null);
+    }
+
+    s3Wrapper.download({Bucket: bucketName, Key: s3Object.key}).then(function(response) {
+        resizer.resize(response.Body).then(function(buffer) {
+            if(!buffer) {
+                console.info('File is already in the expected size');
+                callback(null);
+            } else {
+                s3Wrapper.upload({
+                    Bucket: bucketName,
+                    Key: s3Object.key,
+                    Body: buffer,
+                    ContentType: 'image/jpeg'
+                }).then(function(data) {
+                    console.info('Image uploaded!');
+                    callback(null);
+                }).catch(function(err) {
+                    console.error('Image upload error');
+                    callback(err);
+                });
+            }
+
+        }).catch(function(err) {
+            callback(err);
+        });
+    }).catch(function(err) {
+        callback(err);
+    });
+};
